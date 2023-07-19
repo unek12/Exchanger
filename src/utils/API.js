dea4b9d34f68288b4a052879858e14da03226f0a
Egor Rudman <rudman.14@mail.ru>
2023-07-19 23:25:36 +0300

    changed api key

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,15 @@
 class API {
     static #APIUrl = "https://api.fastforex.io"
+    static #APIKey = "7f3cf67202-78c70631da-rc4jiu"
 
-    static Currencies = async () => await fetch(this.#APIUrl + "/fetch-all?api_key=fb2e799ccf-c26a397f58-r9o4vr")
+    static Currencies = async () => await fetch(this.#APIUrl + "/fetch-all?api_key=" + this.#APIKey)
         .then(res => res.json()).then(res => res["results"])
-    static CurrenciesLabels = async () => await fetch(this.#APIUrl + "/currencies?api_key=fb2e799ccf-c26a397f58-r9o4vr")
+    static CurrenciesLabels = async () => await fetch(this.#APIUrl + "/currencies?api_key=" + this.#APIKey)
         .then(res => res.json()).then(res => res["currencies"])
-    static YesterdayCurrency = async () => await fetch(this.#APIUrl + `/historical?date=${new Date(new Date().setDate(new Date().getDate() - 1)).toLocaleDateString("en-GB").split('/').reverse().join('-')}&from=USD&api_key=fb2e799ccf-c26a397f58-r9o4vr`)
+    static YesterdayCurrency = async () =>
+      await fetch(this.#APIUrl
+        + `/historical?date=${new Date(new Date().setDate(new Date().getDate() - 1)).toLocaleDateString("en-GB").split('/').reverse().join('-')}&from=USD&api_key=`
+        + this.#APIKey)
         .then(res => res.json()).then(res => res["results"])
     static CurrenciesSymbols = require("./CurrencySymbols.json")
 }
